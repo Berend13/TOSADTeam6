11ca462de3df9206f5481bc9f435da555b55f863
Kadeem Vers <kadeemv@outlook.com>
2018-12-11 13:45:29 +0100

    start maven project

---
+++ src/main/java/BRG/BRG_ART_ID/App.java
@@ -0,0 +1,13 @@
+package BRG.BRG_ART_ID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
