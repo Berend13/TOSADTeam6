333c95a2803412aeae3dc700e4ea64334972fc17
berend013 <berend.sliedrecht@student.hu.nl>
2019-01-16 18:51:49 +0100

    object 1, k.

--- src/main/java/BRG/BRG_ART_ID/dao/BusinessRuleDaoImpl.java
+++ src/main/java/BRG/BRG_ART_ID/dao/BusinessRuleDaoImpl.java
@@ -6,6 +6,8 @@ import java.util.*;
 
 import BRG.BRG_ART_ID.domain.BusinessRule;
 
+import javax.print.DocFlavor;
+
 public class BusinessRuleDaoImpl extends BaseDao implements BusinessRuleDao{
 	private static Connection conn;
 	private List<BusinessRule> rules = new ArrayList<BusinessRule>();
@@ -34,8 +36,16 @@ public class BusinessRuleDaoImpl extends BaseDao implements BusinessRuleDao{
 
 	public boolean saveBusinessRule(String BusinessFunction, String BusinessName, String BusinessTable, String BusinessColumn, int BusinessValue1, String BusinessRule, int BusinessValue2, String BusinessError) throws SQLException {
 		conn = BaseDao.getConnection();
-		String query = "begin execute immediate ?(?, ?, ?, ?, ?, ?, ?) end";
+		System.out.println(BusinessFunction);
+		System.out.println(BusinessName);
+		System.out.println(BusinessTable);
+		System.out.println(BusinessColumn);
+		System.out.println(BusinessValue1);
+		System.out.println(BusinessRule);
+		System.out.println(BusinessValue2);
+		System.out.println(BusinessError);
 
+		String query = "begin execute immediate ?(?, ?, ?, ?, ?, ?, ?); end;";
 		PreparedStatement statement = conn.prepareStatement(query);
 		statement.setString(1, BusinessFunction);
 		statement.setString(2, BusinessName);
@@ -46,8 +56,10 @@ public class BusinessRuleDaoImpl extends BaseDao implements BusinessRuleDao{
 		statement.setInt(7, BusinessValue2);
 		statement.setString(8, BusinessError);
 
-		int executeFunction = statement.executeUpdate();
-		if (executeFunction > 0) {
+		System.out.println(statement.toString());
+
+		boolean executeFunction = statement.execute();
+		if (executeFunction == true) {
 			System.out.println("Business rule aangemaakt");
 		}
 		conn.close();		
@@ -55,19 +67,19 @@ public class BusinessRuleDaoImpl extends BaseDao implements BusinessRuleDao{
 	}
 
 	public boolean save(BusinessRule rule) throws SQLException {
-		conn = BaseDao.getConnection();
-		String query = "'begin execute immediate ?(?,?,?,?,?,?,?,?) end;";
-
-		PreparedStatement statement = conn.prepareStatement(query);
-		statement.setString(1, rule.getName());
-		statement.setString(2, rule.getCode());
-		statement.setString(3, rule.getErrorMessage());
-
-		int rowsInserted = statement.executeUpdate();
-		if (rowsInserted > 0) {
-			System.out.println("Artikel toegevoegd");
-		}
-		conn.close();
+//		conn = BaseDao.getConnection();
+//		String query = "begin execute immediate ?(?,?,?,?,?,?,?,?) end;";
+//
+//		PreparedStatement statement = conn.prepareStatement(query);
+//		statement.setString(1, rule.getName());
+//		statement.setString(2, rule.getCode());
+//		statement.setString(3, rule.getErrorMessage());
+//
+//		int rowsInserted = statement.executeUpdate();
+//		if (rowsInserted > 0) {
+//			System.out.println("Artikel toegevoegd");
+//		}
+//		conn.close();
 		return true;
 	}
 
